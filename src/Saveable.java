e0f38a96bf2880d76601f252fa31658ddc0374a2
Paul <p.a.m.van.berkel@gmail.com>
2019-08-06 22:00:57 +0200

    created saveable

---
+++ src/Saveable.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Saveable implements ISaveable {
+
+    public Saveable(List values) {
+    }
+
+    @Override
+    public List values() {
+        return null;
+    }
+}
